28cbced70d8a787fa33b451d443020f66a0ef5bd
unknown <luzhan@bilibili.com>
2024-05-22 12:17:22 +0800

    first

---
+++ src/components/Empty.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    界面
+  </div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Empty",
+  data() {
+    return {};
+  },
+  mounted() {
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+
+</style>
